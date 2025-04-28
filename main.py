c4b23164fbf5ba86769ada6f010c4aec543b42c0
mankala sharathchandra <152910893+sharath4444@users.noreply.github.com>
2025-04-28 19:40:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
